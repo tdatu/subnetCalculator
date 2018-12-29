f88883c030fc278262689b50d5e202d79b97b5c9
tdatu <atdatu@gmail.com>
2018-12-29 12:20:31 -0800

    added favicon

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function updateSubnetMaskView(){
 
 //Register the event listeners when updating IPv4
 ip.forEach(function(el){
-	el.addEventListener("keyup", function(){
+	el.addEventListener("onchange", function(){
 		var octets = [];
 		for(var i = 0; i < ip.length; i++){
 			octets.push(parseInt(ip[i].value));
@@ -59,7 +59,7 @@ ip.forEach(function(el){
 });
 
 //Register event listener when updating CIDR
-cidr.addEventListener("keyup", function(){
+cidr.addEventListener("onchange", function(){
 	app.setCidr(parseInt(this.value));
 	updateTotalIPView();
 	updateBinarySubnetView();
--- index.php
+++ index.php
@@ -4,6 +4,30 @@
 	<meta name="description" content="Networking tools such as NAT IP discovery and Subnet calculator, IP calculator, decimal to binary">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
+
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="icon" sizes="16x16 32x32 64x64" href="/favicon.ico">
+	<link rel="icon" type="image/png" sizes="196x196" href="/favicon-192.png">
+	<link rel="icon" type="image/png" sizes="160x160" href="/favicon-160.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96.png">
+	<link rel="icon" type="image/png" sizes="64x64" href="/favicon-64.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png">
+	<link rel="apple-touch-icon" href="/favicon-57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/favicon-114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/favicon-72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/favicon-144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/favicon-60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/favicon-120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/favicon-76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/favicon-152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon-180.png">
+	<meta name="msapplication-TileColor" content="#FFFFFF">
+	<meta name="msapplication-TileImage" content="/favicon-144.png">
+	<meta name="msapplication-config" content="/browserconfig.xml">
+
+
+
 	<style>
 		
 		input.ip-octet, .cidr, 
@@ -83,7 +107,7 @@
 			<div class="col-md subnet-binary"></div>
 		</div>
 		<div class="row">
-			<div class="col col-md-2">Netmask: </div>
+			<div class="col col-md-2">Mask: </div>
 			<div class="col-md subnet-mask"></div>
 		</div>
 
